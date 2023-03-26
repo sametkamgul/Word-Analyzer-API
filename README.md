a0183644271798300fe3b0b0263da029f10fee07
Samet Kamgul <sametkamgul@gmail.com>
2023-03-27 01:39:10 +0300

    method implementations
    - bad language checker
    - language detection
    - word counter
    - character counter
    - hashtag finder
    - text finder

---
+++ README.md
@@ -0,0 +1,26 @@
+# words analyzer API:  girilen cümle üzerinde analiz yapılması.
+## features
+- bad word analyze
+- how many words and character does it have
+— number of repitition of words
+- number of repitition of character
+- used special characters
+— does it have hashtag
+- finding a character or word in the text.
+- removing character or word from text
+- replacing character or word with desired one
+- language detection
+
+## installation
+````
+npm i
+````
+or
+````
+npm install
+````
+
+
+## TODO:
+- units tests
+- maybe using franc for language detection nlp package
---
+++ server.js
@@ -0,0 +1,18 @@
+const badWordsChecker = require('./lib/badWords');
+const languageDetection = require('./lib/languageDetection');
+const generalHelper = require('./lib/general');
+
+const inputText = 'Thank you very much';
+const textWithBadWords = 'I am not an asshole'
+const textWithHashtag = 'It is a text with #hashtag with another #one. Even with the third one #forallworld';
+
+console.log('bad word check:', badWordsChecker.hasBadWord(textWithBadWords));
+
+languageDetection.detect(inputText).then((result) => {
+    console.log('language detection', result);
+});
+
+console.log('word counter', generalHelper.wordCounter(inputText));
+console.log('char counter', generalHelper.charCounter(inputText));
+console.log('find hashtag', generalHelper.findHashtag(textWithHashtag));
+console.log('find text', generalHelper.findText(textWithHashtag, 'Even'));
