714ca29ab3925196a86b4c40d4ad09dd230127de
Samet Kamgul <sametkamgul@gmail.com>
2023-04-06 02:31:00 +0300

    netlify implementation
    - netlify.toml is added
    - netlify-lambda package is added
    - file structure is changed

--- src/api.js
+++ src/api.js
@@ -7,12 +7,15 @@ const search = require('./routes/search');
 
 const app = new express();
 
+const router = express.Router();
+
 // parsing json in put/post requests
 app.use(express.json());
 
-app.use('/analyze', analyze);
-app.use('/update', update);
-app.use('/search', search);
+app.use('/.netlify/functions/api/analyze', analyze);
+app.use('/.netlify/functions/api/update', update);
+app.use('/.netlify/functions/api/search', search);
 app.use('/*', notAllowed);
 
+module.exports = app;
 module.exports.handler = serverless(app);
