e08c23de5a6e649bb71ba00c09be357a5daf887c
Samet Kamgul <sametkamgul@gmail.com>
2023-04-11 16:57:47 +0300

    /update endpoint major changes
    - /characters and /words sub-endpoints are removed and combined in the root endpoint.
    - helper method is minimized for solid.

--- src/lib/helpers/updateHelper.js
+++ src/lib/helpers/updateHelper.js
@@ -1,88 +1,54 @@
 const generalHelper = require('./generalHelper');
 
 /**
- * removes character(s) from given string
+ * removes character(s) or word(s) from given string
  *
  * @param {String} text - input text
- * @param {String} characters - single or multiple characters seperated by comma to be removed
+ * @param {String} keywords - single or multiple characters or words seperated by comma to be removed
  * @return {String} - updated text will be returned
  */
-const removeCharacters = (text, characters) => {
-    let result = '';
+const remove = (text, keywords) => {
+    let result = text;
 
-    charactersArray = characters.split(',');
+    keywordArray = keywords.split(',');
 
-    charactersArray.forEach((ch) => {
-        result = text.replaceAll(ch, '');
+    // remove whitespace
+    if (keywordArray.length > 0) {
+        keywordArray = keywordArray.map((m) => m.trim());
+    }
+
+    keywordArray.forEach((ch) => {
+        result = result.replaceAll(ch, '');
+    });
+
+    return result;
+};
+
+/**
+ * replace character(s) or word(s) from given string
+ *
+ * @param {String} text - input text
+ * @param {String} keywords - single or multiple characters  or words seperated by comma to be removed
+ * @return {String} - updated text will be returned
+ */
+const replace = (text, keywords, newKeyword) => {
+    let result = text;
+
+    keywordArray = keywords.split(',');
+
+    // remove whitespace
+    if (keywordArray.length > 0) {
+        keywordArray = keywordArray.map((m) => m.trim());
+    }
+
+    keywordArray.forEach((ch) => {
+        result = result.replaceAll(ch, newKeyword);
     });
 
     return result;
 };
 
 module.exports = {
-    removeCharacters,
-};
-
-/**
- * replace character(s) from given string
- *
- * @param {String} text - input text
- * @param {String} characters - single or multiple characters seperated by comma to be removed
- * @return {String} - updated text will be returned
- */
-const replaceCharacters = (text, characters, newCharacter) => {
-    let result = text;
-
-    charactersArray = characters.split(',');
-
-    charactersArray.forEach((ch) => {
-        result = result.replaceAll(ch, newCharacter);
-    });
-
-    return result;
-};
-
-/**
- * removes word(s) from given string
- *
- * @param {String} text - input text
- * @param {String} words - single or multiple words seperated by comma to be removed
- * @return {String} - updated text will be returned
- */
-const removeWords = (text, words) => {
-    let result = '';
-
-    wordsArray = words.split(',');
-
-    wordsArray.forEach((ch) => {
-        result = text.replaceAll(ch, '');
-    });
-
-    return result;
-};
-
-/**
- * replace word(s) from given string
- *
- * @param {String} text - input text
- * @param {String} words - single or multiple words seperated by comma to be removed
- * @return {String} - updated text will be returned
- */
-const replaceWords = (text, words, newWord) => {
-    let result = text;
-
-    wordsArray = words.split(',');
-
-    wordsArray.forEach((w) => {
-        result = result.replaceAll(w, newWord);
-    });
-
-    return result;
-};
-
-module.exports = {
-    removeCharacters,
-    replaceCharacters,
-    removeWords,
-    replaceWords,
+    remove,
+    replace,
 };
--- src/routes/update.js
+++ src/routes/update.js
@@ -4,29 +4,26 @@ const update = require('../lib/helpers/updateHelper');
 
 const router = express.Router();
 
-router.post('/characters', (req, res) => {
+router.post('/', (req, res) => {
     let result = {};
     let statusCode;
 
-    console.log('/update/characters is requested');
+    console.log('/update/ is requested');
 
     const data = req.body.text;
-    const characters = req.body.characters;
-    const newCharacter = req.body.newCharacter;
+    const keywords = req.body.keywords;
+    const newKeyword = req.body.newKeyword;
 
     // assign requested parameters to the response object
-    result.characters = characters;
-    result.newCharacter = newCharacter;
+    result.keywords = keywords;
+    result.newKeyword = newKeyword;
 
-    if (!isEmpty(data)) {
-        if (!isEmpty(newCharacter)) {
-            result.text = update.replaceCharacters(
-                data,
-                characters,
-                newCharacter
-            );
+    // null check
+    if (!isEmpty(data) && !isEmpty(keywords)) {
+        if (!isEmpty(newKeyword)) {
+            result.text = update.replace(data, keywords, newKeyword);
         } else {
-            result.text = update.removeCharacters(data, characters);
+            result.text = update.remove(data, keywords);
         }
 
         statusCode = 200;
@@ -40,41 +37,6 @@ router.post('/characters', (req, res) => {
     }
 
     res.status(statusCode).json(result);
-});
-
-router.post('/words', (req, res) => {
-    let result = {
-        text: '',
-        words: '',
-        newWord: '',
-    };
-
-    console.log('/update/words is requested');
-
-    const data = req.body.text;
-    const words = req.body.words;
-    const newWord = req.body.newWord;
-
-    // assign requested parameters to the response object
-    result.words = words;
-    result.newWord = newWord;
-
-    if (data !== null && data !== undefined && data !== '') {
-        if (newWord !== null && newWord !== undefined && newWord !== '') {
-            result.text = update.replaceWords(data, words, newWord);
-        } else {
-            result.text = update.removeWords(data, words);
-        }
-
-        res.status(200).json(result);
-
-        return;
-    }
-
-    res.status(400).json({
-        status: 'error',
-        message: '<text> parameter is missing in the request of the body!',
-    });
 });
 
 module.exports = router;
